dea109fac7f6180af3a3adcb6e0a05e37157d45b
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-07 12:50:55 +0800

    change import from Link to NavLink

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,49 +1,35 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Divider from '../divider/Divider'
+import { NavLink } from 'react-router-dom'
+import Divider from '../Divider'
 import SidebarData from './SidebarData'
 import logo from '../../assets/logo.svg'
 import { IconContext } from 'react-icons'
 
 const Sidebar = () => {
-  const [sidebarMenu, setSidebarMenu] = useState({
-    activeItem: SidebarData[0],
-    items: SidebarData
-  })
-  const toggleActive = (index) => {
-    setSidebarMenu({...sidebarMenu, activeItem: sidebarMenu.items[index]})
-    console.log(index);
-  }
-
-  const setActive = (index) => {
-    if (sidebarMenu.items[index] === sidebarMenu.activeItem) return 'active-item'
-    // console.log(sidebarMenu.activeItem[index]);
-    console.log(sidebarMenu.activeItem);
-    return ''
-  }
-
   return (
     <>
-      <div className='sidebar'>
+      <div className='fixed top-0 left-0 prm-bg text-white w-64 h-screen m-0 overflow-auto'>
 
-        <header>
-          <div>
+        <header className='prm-bg fixed h-auto w-64'>
+          <div className='flex pt-4'>
             <img src={logo} alt="" className='App-logo' />
-            <span className='header-title'>REACT DASHBOARD</span>
+            <span className='text-lg font-bold sec-text hover:text-sky-400 header-title'>REACT DASHBOARD</span>
           </div>
           <Divider />
         </header>
 
-        <div className='sidebar-items'>
+        <div className='h-[calc(100vh-8rem)] mt-28 ml-3 mr-3 mb-4 sec-text'>
           {
-            sidebarMenu.items.map((item, index) => (
+            SidebarData.map((item, index) => (
               <IconContext.Provider key={index} value={{ className: "mt-auto mb-auto text-xl" }}>
-                <Link key={index} to={item.path} className='sidebar-link' onClick={() => toggleActive(index)}>
-                  <li className={setActive(index)}>
-                    {item.icon}
-                    <span className='ml-4'>{item.title}</span>
-                  </li>
-                </Link>
+                <NavLink
+                  key={index}
+                  to={item.path}
+                  className={`flex hover:text-sky-900 hover:bg-sky-400 mb-2 p-2 rounded-md transition-1 focus:outline-none`}
+                  
+                >
+                  {item.icon}
+                  <span className='ml-4'>{item.title}</span>
+                </NavLink>
               </IconContext.Provider>
             ))
           }
