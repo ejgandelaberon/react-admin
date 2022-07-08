ae624a619c81c665e750ca1a2ee5754cf838c683
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-08 12:38:47 +0800

    edit routes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,32 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../pages/Home'
 import Reports from '../pages/Reports'
-import Users from '../pages/users/Users'
 import CatRoute from '../routes/CatRoute'
+import UserRoute from '../routes/UserRoute'
+import Users from '../pages/users/Users'
+import CreateUser from '../pages/users/CreateUser'
+import PageNotFound from './PageNotFound'
 
 const Content = () => {
   return (
-    <div className='bg-white mt-[5.5rem] ml-[17rem] mr-4 mb-4 p-4 min-h-[calc(100vh-6.5rem)] min-w-min h-auto prm-text rounded-md'>
+    <div 
+      className='mt-[5.5rem] ml-[17rem] mr-4 mb-4
+      min-h-[calc(100vh-6.5rem)] min-w-min h-auto prm-text'
+    >
       <Routes>
-        <Route path='/' element={<Navigate to='home'/>}/>
+        <Route index element={<Home/>}/>
         <Route path='home' element={<Home/>}/>
-        <Route path='cats' element={<CatRoute/>}/>
+
+        <Route path='cats/*' element={<CatRoute/>}/>
+
         <Route path='reports' element={<Reports/>}/>
-        <Route path='users' element={<Users/>}/>
+
+        <Route path='users' element={<UserRoute/>}>
+          <Route index element={<Users/>}/>
+          <Route path='create' element={<CreateUser/>}/>
+        </Route>
+
+        <Route path='*' element={<PageNotFound/>}/>
       </Routes>
     </div>
   )
