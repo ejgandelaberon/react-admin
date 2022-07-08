992434bcf337a67797d0e3058afd0e3c71130b2e
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-08 12:39:23 +0800

    create route container

---
+++ src/components/RouteContainer.jsx
@@ -0,0 +1,9 @@
+const RouteContainer = ({ children }) => {
+  return (
+    <div className="bg-white p-4 rounded-md min-h-[calc(100vh-11rem)]">
+      {children}
+    </div>
+  )
+}
+
+export default RouteContainer
