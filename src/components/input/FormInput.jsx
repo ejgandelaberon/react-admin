3208860625ea548bc1285e6d2c1d2fdeae981def
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-08 19:13:38 +0800

    add properties

--- src/components/input/FormInput.jsx
+++ src/components/input/FormInput.jsx
@@ -1,8 +1,8 @@
-const FormInput = ({ label = 'Label', inputType = 'text', placeholder, refData }) => {
+const FormInput = ({ label = 'Label', inputType = 'text', placeholder, refData, valueData, onChangeData }) => {
   return (
     <div className='flex flex-col grow gap-1'>
       <label className="prm-text prm-hover text-sm  transition-1" htmlFor="">{label}</label>
-      <input ref={refData} className="prm-text border-focus px-2 py-2 rounded-lg border-[1px] border-sky-300"
+      <input ref={refData} value={valueData} onChange={onChangeData} className="prm-text border-focus px-2 py-2 rounded-lg border-[1px] border-sky-300"
         type={inputType} placeholder={placeholder} />
     </div>
   )
